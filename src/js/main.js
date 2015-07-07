2d728bf152adcd4e4b8e09b6b4b0dba2f7a4a672
usehotkey <igolopolosov@gmail.com>
2015-07-07 22:08:33 +0300

    Light site structure

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,18 @@ app.config(function ($routeProvider, $locationProvider) {
         templateUrl: "views/mainPage.html",
         controller: "MainCtrl",
         controllerAs: "main"
+    }).when("/program", {
+        templateUrl: "views/programPage.html",
+        controller: "ProgramCtrl",
+        controllerAs: "program"
+    }).when("/guide", {
+        templateUrl: "views/guidePage.html",
+        controller: "GuideCtrl",
+        controllerAs: "guide"
+    }).when("/contacts", {
+        templateUrl: "views/contactsPage.html",
+        controller: "ContactsCtrl",
+        controllerAs: "contacts"
     }).otherwise({
         redirectTo: '/'
     });
