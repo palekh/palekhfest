d6a88fd241681cc57394e300da4cf4d0c4624bbc
usehotkey <igolopolosov@gmail.com>
2015-07-12 16:44:03 +0300

    Made contest

--- src/js/app.js
+++ src/js/app.js
@@ -2,29 +2,36 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider.when("/", {
-        templateUrl: "views/mainPage.html",
+        templateUrl: "views/main.html",
         controller: "MainCtrl",
         controllerAs: "main"
     }).when("/program", {
-        templateUrl: "views/programPage.html",
+        templateUrl: "views/program.html",
         controller: "ProgramCtrl",
         controllerAs: "program"
     }).when("/guide", {
-        templateUrl: "views/guidePage.html",
-        controller: "GuideCtrl",
-        controllerAs: "guide"
+        templateUrl: "views/guide/guide.html"
+    }).when("/guide/howtoget", {
+        templateUrl: "views/guide/howtoget.html"
     }).when("/contacts", {
-        templateUrl: "views/contactsPage.html",
+        templateUrl: "views/contacts.html",
         controller: "ContactsCtrl",
         controllerAs: "contacts"
     })
         .when("/contest", {
-            templateUrl: "views/contestPage.html",
-            controller: "ContestCtrl",
-            controllerAs: "contest"
+            redirectTo: "/contest/about"
+        })
+        .when("/contest/about", {
+            templateUrl: "views/contest/about.html"
+        })
+        .when("/contest/fairytale", {
+            templateUrl: "views/contest/fairytale.html"
+        })
+        .when("/contest/rules", {
+            templateUrl: "views/contest/rules.html"
         })
         .when("/palekh", {
-            templateUrl: "views/contactsPage.html",
+            templateUrl: "views/contacts.html",
             controller: "PalekhCtrl",
             controllerAs: "palekh"
         }).otherwise({
