362cb78a4cf6cbb046f9d4eed1b20c8d99a61046
usehotkey <igolopolosov@gmail.com>
2015-07-18 18:13:08 +0300

    mobile

--- src/js/app.js
+++ src/js/app.js
@@ -2,21 +2,21 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider.when("/", {
-        templateUrl: "views/main.html",
-        controller: "MainCtrl",
-        controllerAs: "main"
+        templateUrl: "views/main.html"
     }).when("/program", {
-        templateUrl: "views/program.html",
+        templateUrl: "views/program/program.html",
         controller: "ProgramCtrl",
         controllerAs: "program"
+    }).when("/program/participants", {
+        templateUrl: "views/program/participants.html",
+        controller: "ParticipantsCtrl",
+        controllerAs: "participants"
     }).when("/guide", {
         templateUrl: "views/guide/guide.html"
     }).when("/guide/howtoget", {
         templateUrl: "views/guide/howtoget.html"
     }).when("/contacts", {
-        templateUrl: "views/contacts.html",
-        controller: "ContactsCtrl",
-        controllerAs: "contacts"
+        templateUrl: "views/contacts.html"
     })
         .when("/contest", {
             redirectTo: "/contest/about"
@@ -35,4 +35,10 @@ app.config(function ($routeProvider) {
         }).otherwise({
         redirectTo: '/'
     });
+});
+
+app.controller('IntroCtrl', function ($location) {
+    this.isMain = function () {
+        return $location.path() === '/';
+    }
 });
