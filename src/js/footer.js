aeca90e24390d2892045e0069c401a89d0b39b5b
Igor Golopolosov <igolopolosov@gmail.com>
2016-01-05 01:36:56 +0300

    Optimize images to indexing by search engines. Partner logos, footer animals, main picture.

--- src/js/footer.js
+++ src/js/footer.js
@@ -2,13 +2,25 @@ app.directive("appleFooter", function ($rootScope) {
     return {
         restrict: "E",
         templateUrl: "views/elements/footer.html",
-        link: function ($scope, element, attrs) {
+        link: function ($scope, element) {
             $rootScope.$on("$routeChangeSuccess", function () {
-                var backgrounds = ["../img/footer-bird.png",
-                    "../img/footer-cat.png"];
-                element.find('.animal').css({
-                    'background-image': 'url(' + backgrounds[Math.floor(Math.random() * backgrounds.length)] + ')'
-                });
+                var backgrounds = [
+                    {
+                        "src":"../img/footer-bird.png",
+                        "alt":"Курица клюёт крошки. Иллюстрация",
+                        "title": "Курочка Крошка"
+                    },
+                    {
+                        "src": "../img/footer-cat.png",
+                        "alt": "Весёлый кот. Иллюстрация",
+                        "title": "Кот Офей"
+                    },
+                ];
+                var randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+                var elemAnimal = element.find('.animal');
+                for (var attr in randomBg){
+                    elemAnimal.attr(attr, randomBg[attr]);
+                }
             });
         }
     };
