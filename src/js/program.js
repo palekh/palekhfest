082f2620af80eebaa2d9cf6d8e7cbd994c38ab96
Dmitry Romashov <FrameBassman@gmail.com>
2017-12-01 12:38:50 +0300

    Initial commit

--- src/js/program.js
+++
@@ -1,9 +0,0 @@
-app.controller('ProgramCtrl', function ($scope, $http) {
-
-    $http.get('json/program.json').success(function (data, status, headers, config) {
-        $scope.days = data;
-    }).error(function (data, status, headers, config) {
-            console.log('error');
-        });
-	
-});
