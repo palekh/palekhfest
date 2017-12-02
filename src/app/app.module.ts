d477eb5fa15cdcd0a8e8cbb8136da4db8df88d58
Dmitry Romashov <FrameBassman@gmail.com>
2017-12-02 10:08:52 +0300

    Add assets and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProgramComponent } from './program/program.component';
 import { ParticipantsComponent } from './participants/participants.component';
 import { GuideComponent } from './guide/guide.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
+const routes = [
+  {path: '', component: HomeComponent},
+  {path: 'program', component: ProgramComponent},
+  {path: 'participants', component: ParticipantsComponent},
+  {path: 'guide', component: GuideComponent},
+  {path: 'contacts', component: ContactsComponent},
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +25,14 @@ import { ContactsComponent } from './contacts/contacts.component';
     ProgramComponent,
     ParticipantsComponent,
     GuideComponent,
-    ContactsComponent
+    ContactsComponent,
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
