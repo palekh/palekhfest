5111205f0473554c5c738ee283708f164f470df2
Igor Golopolosov <igolopolosov@gmail.com>
2020-03-08 21:40:35 +0000

    :bug: Fix selecting animal on footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,25 +1,30 @@
+
+var animalBackgrounds = [
+    {
+        "src":"../img/footer-bird.png",
+        "alt":"Курица клюёт крошки. Иллюстрация",
+        "title": "Курочка Крошка"
+    },
+    {
+        "src": "../img/footer-cat.png",
+        "alt": "Весёлый кот. Иллюстрация",
+        "title": "Кот Офей"
+    },
+];
+
 app.directive("appleFooter", function ($rootScope) {
     return {
         restrict: "E",
         templateUrl: "views/elements/footer.html",
-        link: function ($scope, element) {
+        link: function () {
             $rootScope.$on("$routeChangeSuccess", function () {
-                var backgrounds = [
-                    {
-                        "src":"../img/footer-bird.png",
-                        "alt":"Курица клюёт крошки. Иллюстрация",
-                        "title": "Курочка Крошка"
-                    },
-                    {
-                        "src": "../img/footer-cat.png",
-                        "alt": "Весёлый кот. Иллюстрация",
-                        "title": "Кот Офей"
-                    },
-                ];
-                var randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-                var elemAnimal = element.find('.animal');
-                for (var attr in randomBg){
-                    elemAnimal.attr(attr, randomBg[attr]);
+                const index = Math.random() > 0.5
+                    ? 1
+                    : 0;
+                var randomAnimal = animalBackgrounds[index];
+                var animalElement = document.getElementsByClassName('animal')[0];
+                for (var attribute in randomAnimal) {
+                    animalElement.setAttribute(attribute, randomAnimal[attribute]);
                 }
             });
         }
